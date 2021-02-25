c6b15a4b4a80875fe0a56986b2e633d4f44c1a4a
diegofch29 <diegofch29@hotmail.com>
2021-02-25 14:48:50 -0500

    Login Ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './componets/Login.js';
+import Tasks from './componets/Tasks.js';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const userData = {
+        userName : "diego@gmail.com",
+        password : "1234"
+    };
+    
+    
+    localStorage.setItem("Username", userData.userName);
+    localStorage.setItem("Password", userData.password);
+    localStorage.setItem("isLoggedIn", false);
+    
+    let isLoggedIn = false;
+    
+    const [isLoggedInValue,setIsLoggedIn]=useState(isLoggedIn);
+    
+    const handleSuccessfullyLogin = (e) => {
+        setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", true);
+        window.location.href = "/home";
+    };
+    
+    const handleFailedLogin = (e) => {
+        alert("Usuario o Clave Incorrectos");
+        setIsLoggedIn(false);
+        localStorage.setItem("isLoggedIn", false);
+        window.location.href = "/";
+    };
+    
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        localStorage.setItem("isLoggedIn", false);
+        window.location.href = "/";
+    };
+    
+    const items=[];
+    
+    
+    const LoginView = () => (<Login successfully={handleSuccessfullyLogin} failed={handleFailedLogin}/>);
+    const HomeView = () => (<Tasks items={items} logout={handleLogout} email={userData.email} name={userData.username}/>);
+    
+    
+    return (
+          <Router>
+            <div className="App">
+                <Route exact path="/" component={LoginView}/>
+                <Route path="/home" component={HomeView}/>
+            </div>
+        </Router>
   );
 }
 
